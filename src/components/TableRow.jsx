0a6813daaa707658dbe8ecd393984ed797c3b909
Wissam <riki_kabha@hotmail.com>
2022-09-02 01:02:48 +0300

    final

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
 
 function TableRow(props) {
-  const { rank, name, priceUsd, symbol, maxSupply, changePercent24Hr } =
+  const { rank, name, priceUsd, symbol, supply, changePercent24Hr } =
     props.row;
 
+function priceUpdate(){
+  var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+ 
+  });
+  return formatter.format(priceUsd)
+}
+
+
   return (
-    <tr>
+    <tr className="row">
       <td>{rank}</td>
-      <td>
+      <td className="name">
         {name + " "} <span>{symbol}</span>
       </td>
-      <td>{"$" + priceUsd}</td>
-      <td>{changePercent24Hr}</td>
-      <td>{maxSupply}</td>
+      <td>{ priceUpdate()}</td>
+      <td className={
+       changePercent24Hr>0?"postive":"negative"
+      }> <i className={`fa fa-sort-${changePercent24Hr>0? "asc" : "desc"}`}></i>{changePercent24Hr.substring(0,7)}</td>
+      <td>{ `${supply}`}</td>
     </tr>
   );
 }
